d4ad77688e541aafefdcb1b26197d8af51549585
drm2 <davidrmyersii@gmail.com>
2016-10-18 14:09:59 -0400

    Reorganizing code to allow a reminder to be scheduled outside of the normal 'stream' flow. Implementing reminder persistence. Adding timezone abbreviation to timestamp string.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var TwitterBot = require('@drm2/twitterbot');
 var dotenv = require('dotenv');
 var HumanDate = require('date.js');
+var Store = require('jfs');
 
 // load our environment variables
 dotenv.config();
@@ -13,6 +14,8 @@ var bot = new TwitterBot({
     access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
 });
 
+var db = new Store('./storage/data.json');
+
 var keyword = '#remindme';
 var months = [ 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec', ];
 
@@ -23,15 +26,60 @@ var format_date = function (date)
     var year = date.getFullYear();
     var hour = ('0' + date.getHours()).slice(-2);
     var minute = ('0' + date.getMinutes()).slice(-2);
+    var date_str = date.toString();
+    var timezone_abbr = date_str.substring(date_str.indexOf('(') + 1, date_str.indexOf(')'));
 
-    return month + ' ' + day + ', ' + year + ' @ ' + hour + ':' + minute;
+    return month + ' ' + day + ', ' + year + ' @ ' + hour + ':' + minute + ' ' + timezone_abbr;
+};
+
+var format_status = function (tweet)
+{
+    return tweet.in_reply_to_screen_name ? `\n\nhttps://twitter.com/${tweet.in_reply_to_screen_name}/status/${tweet.in_reply_to_status_id_str}` : '';
+};
+
+var schedule_reminder = function (tweet, reminder_time)
+{
+    var status = format_status(tweet);
+
+    // schedule the actual reminder reply
+    bot.schedule(function () {
+        var message = `Hey! 'Member this?${status}`;
+
+        bot.reply(message, { screen_name: tweet.user.screen_name, tweet_id: tweet.id_str });
+    }, reminder_time)
+        .then(function () {
+            // // remove reminder from filesystem
+            // storage.removeItem(tweet.id_str);
+            db.delete(tweet.id_str, function () {});
+        });
 };
 
 var stream = bot.filteredStream(keyword);
 
+// // init storage
+// storage.init({
+//     dir: 'storage.json'
+// });
+//
+// // loop through storage and set schedules
+// storage.forEach(function (id, info) {
+//     console.log('id', id);
+//     console.log('info', info);
+// });
+
+db.all(function (err, reminders) {
+    for (var id in reminders)
+    {
+        if (reminders.hasOwnProperty(id))
+        {
+            schedule_reminder(reminders[id].tweet, new Date(reminders[id].reminder_time));
+        }
+    }
+});
+
 stream.on('tweet', function (tweet) {
     // build the link to the original (if one exists)
-    var status = tweet.in_reply_to_screen_name ? `\n\nhttps://twitter.com/${tweet.in_reply_to_screen_name}/status/${tweet.in_reply_to_status_id_str}` : '';
+    var status = format_status(tweet);
 
     // find the reminder delay (provided in the #remindme Tweet)
     var reminder_time = HumanDate(tweet.text.substring(tweet.text.indexOf(keyword) + keyword.length, tweet.text.length));
@@ -45,10 +93,21 @@ stream.on('tweet', function (tweet) {
     // send the initial acknowledgement reply
     bot.reply(message, { screen_name: tweet.user.screen_name, tweet_id: tweet.id_str });
 
-    // schedule the actual reminder reply
-    bot.schedule(function () {
-        var message = `Hey! 'Member this?${status}`;
+    // store reminder on filesystem
+    db.save(tweet.id_str, {
+        tweet: tweet,
+        reminder_time: reminder_time
+    }, function () {});
 
-        bot.reply(message, { screen_name: tweet.user.screen_name, tweet_id: tweet.id_str });
-    }, reminder_time);
+    schedule_reminder(tweet, reminder_time);
+
+    console.log(tweet);
+});
+
+stream.on('connect', function () {
+    console.log('connecting...');
+});
+
+stream.on('connected', function () {
+    console.log('connected!');
 });
